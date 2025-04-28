d042de9f1a79173e3a17aeb635455d8d5a90a75e
hager <hagermahmod@gmail.com>
2025-04-28 06:39:17 +0300

    add a input

--- Delivery/delivery.js
+++ Delivery/delivery.js
@@ -3,6 +3,7 @@ document.getElementsByClassName("Reservation_form")[0].addEventListener("submit"
     var Name = document.getElementsByClassName("Reservation_Name")[0].value;
     var Person = document.getElementsByClassName("Reservation_Person")[0].value;
     var Timing = document.getElementsByClassName("Reservation_Timing")[0].value;
+    var Comming = document.getElementsByClassName("Reservation_Options")[0].value;
     var Date = document.getElementsByClassName("Reservation_Date")[0].value;
 
     let reservationData = localStorage.getItem("reservationData");
@@ -13,7 +14,13 @@ document.getElementsByClassName("Reservation_form")[0].addEventListener("submit"
         reservationData = [];
     }
 
-    reservationData.push({ Name, Person, Timing, Date });
-    localStorage.setItem("reservationData", JSON.stringify(reservationData));
+    // reservationData.push({ Name, Person, Timing, Comming,Date });
+    // localStorage.setItem("reservationData", JSON.stringify(reservationData));
+
+    // var successMessage = document.getElementById('hidden_text');
+    // successMessage.style.display = 'block'; 
+    // setTimeout(function() {
+    //     successMessage.style.display = 'none'; 
+    // }, 3000);
     
 });
